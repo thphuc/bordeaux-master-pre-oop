7499d5c8a4f05a2ce5b2968b311f351c81c3a33d
Phuc Tran <huyphuc0906@gmail.com>
2020-01-03 16:10:30 +0700

    Implement exercise 3

---
+++ src/day_01/Exercise03.java
@@ -0,0 +1,26 @@
+package day_01;
+
+import java.util.Scanner;
+
+/*
+ * Take width and height of a rectangle and print the area and perimeter. 
+ * Test:
+ * Width = 5 Height = 8
+ * Expected Output
+ * Area is 5 * 8 = 40
+ * Perimeter is 2 * (5 + 8) = 26
+*/
+public class Exercise03 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter width: width = ");
+		int width = scanner.nextInt();
+		System.out.print("Enter height: height = ");
+		int height = scanner.nextInt();
+		scanner.close();
+
+		System.out.println(String.format("Width = %d, Height = %d", width, height));
+		System.out.println(String.format("Area is : %d * %d = %d", width, height, (width * height)));
+		System.out.println(String.format("Perimeter is 2 * (%d + %d) = %d", width, height, (2 * (width + height))));
+	}
+}
