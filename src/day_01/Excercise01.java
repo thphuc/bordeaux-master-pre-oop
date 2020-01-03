45d4aa0b0cdf588c34f9f804bde0b6866499a5b5
Phuc Tran <huyphuc0906@gmail.com>
2020-01-03 10:33:28 +0700

    Implement exercise 1 (day 1)

---
+++ src/day_01/Excercise01.java
@@ -0,0 +1,18 @@
+package day_01;
+
+/**
+* 1. Print 'Hello World' on screen and then print your full name and your nationality. 
+* Expected Output :
+* Hello World 
+* Nguyen Minh Duong, Vietnamese.
+* 
+* @author phuctran
+*
+*/
+public class Excercise01 {
+	
+	public static void main() {
+		System.out.println("Hello World");
+		System.out.println("Tran Huy Phuc, Vietnamese");
+	}
+}
